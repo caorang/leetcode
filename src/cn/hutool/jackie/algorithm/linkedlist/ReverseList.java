1bb2d08563f8f7406960793bb9359f5f57fb8ed3
欧布 <rcao1@xiaohongshu.com>
2023-11-24 00:19:43 +0800

    add

--- src/cn/hutool/jackie/algorithm/linkedlist/ReverseList.java
+++ src/cn/hutool/jackie/algorithm/linkedlist/ReverseList.java
@@ -22,7 +22,6 @@ public class ReverseList {
                 sb.append(node.val).append("->");
                 node = node.next;
             }
-            sb.append("null");
             return sb.toString();
         }
     }
@@ -38,7 +37,8 @@ public class ReverseList {
         node3.next = node4;
         node4.next = node5;
         System.out.println(head);
-        System.out.println(new ReverseList().reverseList(head));
+//        System.out.println(new ReverseList().reverseList(head));
+        System.out.println(new ReverseList().reverseBetween(head, 2, 4));
     }
 
     public ListNode reverseList(ListNode head) {
@@ -51,5 +51,41 @@ public class ReverseList {
             cur = next;
         }
         return pre;
+    }
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        // 因为头节点有可能发生变化，使用虚拟头节点可以避免复杂的分类讨论
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+
+        ListNode pre = dummyNode;
+        // 第 1 步：从虚拟头节点走 left - 1 步，来到 left 节点的前一个节点
+        // 建议写在 for 循环里，语义清晰
+        for (int i = 0; i < left - 1; i++) {
+            pre = pre.next;
+        }
+
+        // 第 2 步：从 pre 再走 right - left + 1 步，来到 right 节点
+        ListNode rightNode = pre;
+        for (int i = 0; i < right - left + 1; i++) {
+            rightNode = rightNode.next;
+        }
+
+        // 第 3 步：切断出一个子链表（截取链表）
+        ListNode leftNode = pre.next;
+        ListNode curr = rightNode.next;
+
+        // 注意：切断链接
+        pre.next = null;
+        rightNode.next = null;
+
+        // 第 4 步：同第 206 题，反转链表的子区间
+        reverseList(leftNode);
+
+        // 第 5 步：接回到原来的链表中
+        pre.next = rightNode;
+        leftNode.next = curr;
+
+        return dummyNode.next;
     }
 }

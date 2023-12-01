167e67cda152f7a3d4d8cf9f6655a44114ec8876
欧布 <rcao1@xiaohongshu.com>
2023-12-01 15:13:47 +0800

    DesignPhoneDirectory

--- src/cn/hutool/jackie/algorithm/plus/DesignPhoneDirectory.java
+++ src/cn/hutool/jackie/algorithm/plus/DesignPhoneDirectory.java
@@ -1,5 +1,9 @@
 package cn.hutool.jackie.algorithm.plus;
 
+import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * 设计一个电话目录管理系统，让它支持以下功能：
  * <p>
@@ -47,25 +51,55 @@ package cn.hutool.jackie.algorithm.plus;
 public class DesignPhoneDirectory {
 
     public static void main(String[] args) {
-
+        // 初始化电话目录，包括 3 个电话号码：0，1 和 2。
+        PhoneDirectory directory = new PhoneDirectory(3);
+        // 可以返回任意未分配的号码，这里我们假设它返回 0。
+        System.out.println(directory.get());
+        // 假设，函数返回 1。
+        System.out.println(directory.get());
+        // 号码 2 未分配，所以返回为 true。
+        System.out.println(directory.check(2));
+        // 返回 2，分配后，只剩一个号码未被分配。
+        System.out.println(directory.get());
+        // 此时，号码 2 已经被分配，所以返回 false。
+        System.out.println(directory.check(2));
+        // 释放号码 2，将该号码变回未分配状态。
+        directory.release(2);
+        // 号码 2 现在是未分配状态，所以返回 true。
+        System.out.println(directory.check(2));
     }
 }
 
 class PhoneDirectory {
 
-    public PhoneDirectory(int maxNumbers) {
+    private Map<Integer, Boolean> numbers = new TreeMap<>(Comparator.comparingInt(a -> a.intValue()));
 
+    public PhoneDirectory(int maxNumbers) {
+        for (int i = 0; i < maxNumbers; i++) {
+            numbers.put(i, true);
+        }
     }
 
     public int get() {
-        return 0;
+        for (Map.Entry<Integer, Boolean> entry : numbers.entrySet()) {
+            if (Boolean.TRUE.equals(entry.getValue())) {
+                entry.setValue(false);
+                return entry.getKey();
+            }
+        }
+        return -1;
     }
 
     public boolean check(int number) {
-        return false;
+        if (!numbers.containsKey(number)) {
+            return false;
+        }
+        return numbers.get(number);
     }
 
     public void release(int number) {
-
+        if (numbers.containsKey(number)) {
+            numbers.put(number, true);
+        }
     }
 }
